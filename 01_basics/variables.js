91bf16a7c119cea898d19b767eb724bf234b1849
Ayush Kumar <126838522+Ayush7055@users.noreply.github.com>
2025-02-12 19:49:17 +0000

    understanding the concept of variable

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const adress = 1223344;
+let A = 108
+
+var d = 24
+console.log(adress)
+
+//suggest not to use var because of its block scope and functional scope
+//const keyword is used to define the constant variable whose value can not be changed throught the code
+// always used let keyword because we can reassign the value.
