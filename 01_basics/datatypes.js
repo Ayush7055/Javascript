fd41c743bd4e5dae608d2ba33dcd4efd48ef40c3
Ayush Kumar <126838522+Ayush7055@users.noreply.github.com>
2025-02-12 19:47:30 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+//number
+//bigInt
+//String => ""
+//boolean => true/false
+//undefined 
+//null => standalone value
+//symbol => unique
+
+let a = "Ayush"
+let number = 10
+let isvalue= true
+let state;
+
+
+console.log(typeof state)
