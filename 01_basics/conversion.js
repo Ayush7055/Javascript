0e489d47247379e90ab93931e25edc04043e94db
Ayush Kumar <126838522+Ayush7055@users.noreply.github.com>
2025-02-12 20:37:53 +0000

    how things are converted

---
+++ 01_basics/conversion.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+//console.log(score)
+//console.log(typeof score)
+
+let isScore = Number(score)
+//console.log(isScore)
+//console.log(typeof isScore)
+
+// "33" => 33(number)
+//"33abc" => NaN
+
+let isLoggedIn = true 
+//console.log(isLoggedIn)
+
+let numIsLoggedIn = Number(isLoggedIn)
+//console.log(numIsLoggedIn)
+
+
+
+//true => 1
+//false =>0
+
+let isloggedIn = "Ayush"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ayush" => true
