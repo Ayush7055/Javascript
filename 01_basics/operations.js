5883c6c5e3d50c106c5c77d725293803e4b1a741
Ayush Kumar <126838522+Ayush7055@users.noreply.github.com>
2025-02-12 20:37:23 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,34 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);  -3
+
+// console.log(2+2); 4
+// console.log(2-2); 0
+// console.log(2*2); 4
+// console.log(2**3); 8
+// console.log(2/3); 0.6666
+// console.log(2%3); 1
+
+let str1 = "hello"
+let str2 = " Ayush"
+
+let str3 = str1 + str2
+// console.log(str3); hello Ayush
+
+// console.log("1" + 2); 12
+// console.log(1 + "2"); 12
+// console.log("1" + 2 + 2); 122
+// console.log(1 + 2 + "2"); 32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2//not a good practise
+
+let gameCounter = 100
+++gameCounter; // study prefix and post fix
+console.log(gameCounter);
