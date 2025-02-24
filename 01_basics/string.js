f242999821c5036e63c9d20e35f5b2e2a09bef33
Ayush Kumar <126838522+Ayush7055@users.noreply.github.com>
2025-02-24 18:31:47 +0000

    String functions

---
+++ 01_basics/string.js
@@ -0,0 +1,25 @@
+const name = "Ayush Kumar"
+const instaFollower = 810
+
+console.log(`I am ${name} and I have ${instaFollower} followers on my social media`)
+
+const newStr = new String("hello World")
+
+console.log(newStr)
+console.log(newStr.length)
+console.log(newStr.charAt(2))
+console.log(newStr.toUpperCase)
+
+
+const str = "The quick brown fox jumps over the lazy dog.";
+console.log(str.slice(20))
+
+console.log(str.slice(2,30))
+
+
+
+const paragraph = "I think Ruth's dog is cuter than your dog!";
+
+console.log(paragraph.replace("Ruth's", "my"));
+// Expected output: "I think my dog is cuter than your dog!"
+
